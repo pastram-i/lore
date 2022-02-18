360fa46b3553bcfa355656c7c3b88a8b82e84bd2
pastram-i <73852781+pastram-i@users.noreply.github.com>
2022-02-18 15:39:36 -0600

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const LORE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ LORE.abilities = {
+  "str": "LORE.AbilityStr",
+  "dex": "LORE.AbilityDex",
+  "con": "LORE.AbilityCon",
+  "int": "LORE.AbilityInt",
+  "wis": "LORE.AbilityWis",
+  "cha": "LORE.AbilityCha"
+};
+
+LORE.abilityAbbreviations = {
+  "str": "LORE.AbilityStrAbbr",
+  "dex": "LORE.AbilityDexAbbr",
+  "con": "LORE.AbilityConAbbr",
+  "int": "LORE.AbilityIntAbbr",
+  "wis": "LORE.AbilityWisAbbr",
+  "cha": "LORE.AbilityChaAbbr"
+};
